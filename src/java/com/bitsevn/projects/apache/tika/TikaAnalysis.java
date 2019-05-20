f4a5aa918b467db40c2558cf818f0eac4c55bb17
bitsevn <bitsevn@gmail.com>
2019-05-20 23:35:14 +0530

    rest

--- src/java/com/bitsevn/projects/apache/tika/TikaAnalysis.java
+++ src/java/com/bitsevn/projects/apache/tika/TikaAnalysis.java
@@ -8,19 +8,27 @@ import org.apache.tika.mime.MediaType;
 import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
+import org.apache.tika.parser.ocr.TesseractOCRConfig;
+import org.apache.tika.parser.pdf.PDFParserConfig;
 import org.apache.tika.sax.BodyContentHandler;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class TikaAnalysis {
 
-    private static final String PATH = "D:\\code\\java\\ApacheTika\\docs\\";
+    private static final String PATH = "D:\\code\\java\\ApacheTika\\resources\\docs\\";
 
     public static void main(String[] args) throws TikaException, IOException, SAXException {
-        System.out.println(extractContentUsingParser(getInputStream("docs/file-xml.xml")));
+        System.out.println(extractContentFromPdf(toInputStream("big-pdf.pdf")));
+    }
+
+    public static InputStream toInputStream(String fileName) throws FileNotFoundException {
+        return new FileInputStream(PATH + fileName);
     }
 
     public static InputStream getInputStream(String fileName) {
@@ -54,6 +62,23 @@ public class TikaAnalysis {
         Metadata metadata = new Metadata();
         ParseContext context = new ParseContext();
 
+        parser.parse(stream, handler, metadata, context);
+        return handler.toString();
+    }
+
+    public static String extractContentFromPdf(InputStream stream)
+            throws IOException, TikaException, SAXException {
+
+        Parser parser = new AutoDetectParser();
+        TesseractOCRConfig config = new TesseractOCRConfig();
+        PDFParserConfig pdfConfig = new PDFParserConfig();
+        pdfConfig.setExtractInlineImages(true);
+
+        ContentHandler handler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+        ParseContext context = new ParseContext();
+        context.set(TesseractOCRConfig.class, config);
+        context.set(PDFParserConfig.class, pdfConfig);
         parser.parse(stream, handler, metadata, context);
         return handler.toString();
     }
